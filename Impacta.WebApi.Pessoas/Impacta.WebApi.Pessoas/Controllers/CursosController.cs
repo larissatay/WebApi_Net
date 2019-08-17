0a075389c4ee7882fcd1638c2e84fea644adbeb9
laritay <ldetoni70@gmail.com>
2019-08-17 15:25:55 -0300

    Projeto WebApi aula 17082019

---
+++ Impacta.WebApi.Pessoas/Impacta.WebApi.Pessoas/Controllers/CursosController.cs
@@ -0,0 +1,85 @@
+﻿using Impacta.WebApi.Pessoas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Impacta.WebApi.Pessoas.Controllers
+{
+	public class CursosController : ApiController
+	{
+		static List<Cursos> ListaDeCursos = new List<Cursos>();
+
+		public List<Cursos> GetCrusos()
+		{
+			return ListaDeCursos;
+		}
+
+		public Cursos GetCursos(int Id)
+		{
+			var consultaCurso = from c in ListaDeCursos
+								where c.Id.Equals(Id)
+								select c;
+
+			var res = ListaDeCursos.Where(x => x.Id.Equals(Id)).FirstOrDefault();
+
+			if (consultaCurso.Count() <= 0)
+			{
+				return null;
+			}
+			else
+			{
+				return consultaCurso.FirstOrDefault();
+			}
+
+		}
+
+		public void PostCursos(Cursos curso)
+		{
+			if (curso != null)
+			{
+				ListaDeCursos.Add(curso);
+			}
+		}
+
+		public void PutCursos(Cursos curso)
+		{
+			if (curso != null)
+			{
+				ListaDeCursos.Add(curso);
+			}
+		}
+
+		public void PutCursos(int id, Cursos curso)
+		{
+			if (curso != null && id > 0)
+			{
+				var result = ListaDeCursos.Where(x => x.Id.Equals(id)).FirstOrDefault();
+
+				result.Nome = curso.Nome;
+				result.CargaHoraria = curso.CargaHoraria;
+
+				int posicao = ListaDeCursos.IndexOf(curso);
+				ListaDeCursos.RemoveAt(posicao);
+				ListaDeCursos.Insert(posicao, curso);
+
+			}
+		}
+
+		public List<Cursos> DeleteCursos(int id)
+		{
+			if (id > 0)
+			{
+				ListaDeCursos.RemoveAt(
+					ListaDeCursos.IndexOf(
+						ListaDeCursos.Where(x => x.Id.Equals(id)).FirstOrDefault()
+						));
+			}
+
+			return ListaDeCursos;
+		}
+
+	}
+}
---
+++ Impacta.WebApi.Pessoas/Impacta.WebApi.Pessoas/Controllers/PessoasController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Impacta.WebApi.Pessoas.Models;
+
+namespace Impacta.WebApi.Pessoas.Controllers
+{
+    public class PessoasController : ApiController
+    {
+		// como nao temos ainda o bd, vamos simular utilizando
+		// uma lista, do tipo Static
+		List<Pessoa> pessoas = new List<Pessoa>();
+
+		public PessoasController()
+		{
+
+		}
+
+	
+		public List<Pessoa> Get ()
+		{
+			return pessoas;
+		}
+
+		public void Post(string nomeDaPessoa)
+		{
+			if (!string.IsNullOrEmpty(nomeDaPessoa))
+			{
+				pessoas.Add(new Pessoa { Nome = nomeDaPessoa });
+			}
+		}
+
+		public void Post(Pessoa pessoa)
+		{
+			if (pessoa != null) 
+			{
+				pessoas.Add(pessoa);
+			}
+		}
+    }
+}
