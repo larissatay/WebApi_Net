e6067c3f17c8edcdd402f37595ad8d9d0fc43b3e
laritay <ldetoni70@gmail.com>
2019-08-24 17:03:46 -0300

    Aula dia 24

---
+++ Impacta.Tarefas/Impacta.MOD/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impacta.MOD
+{
+	public class Usuario
+	{
+
+		[Required(ErrorMessage = "Nome do usuario é obrigatorio")]
+		[Display(Name ="USUÁRIO")]
+		[StringLength(10, MinimumLength =5, ErrorMessage =("Usuario deve conter entre 5 e 8 caracters"))]
+		public string Username { get; set; }
+
+		[Display(Name = "SENHA")]
+		[Required(ErrorMessage = "Senha é obrigatória")]
+		public string Password { get; set; }
+	}
+}
---
+++ Impacta.Tarefas/Impacta.Tarefas/Controllers/AutenticadorController.cs
@@ -0,0 +1,39 @@
+﻿using Impacta.MOD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Impacta.Tarefas.Controllers
+{
+    public class AutenticadorController : Controller
+    {
+        // GET: Autenticador
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+		public ActionResult Entar(Usuario usuario)
+		{
+			if (usuario.Username != null && usuario.Password != null && usuario.Username.Equals("Larissa") && usuario.Password.Equals("RealBooks"))
+			{
+				Session["Usuario"] = usuario;
+				return RedirectToAction("Index", "RealBooks");
+			}
+
+		else
+			{
+				ViewBag.Mensagem = "usuário ou senha incorretos";
+				return View("Formulario");
+			}
+		}
+
+		public ActionResult Sair()
+		{
+			Session.Abandon();
+			return RedirectToAction("Formulario", "Autenticador"); 
+		}
+    }
+}
